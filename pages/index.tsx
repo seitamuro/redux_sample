7c12ae8b10ce86527e7659aa73b9e2f386796937
seitamuro <seitamuro0210@gmail.com>
2023-01-08 21:56:56 +0900

    /anotherpageへのリンクを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,18 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
-import store from "../components/app/store"
-import { Provider } from "react-redux"
 import { Counter } from '../components/feature/counter/Counter'
+import Link from "next/link"
 
 export default function Home() {
   return (
-    <Provider store={store}>
-      <Counter />
-    </Provider>
+    <>
+      <div>
+        <Counter />
+      </div>
+      <div>
+        <Link href="/anotherpage">go to <code>/anotherpage</code></Link>
+      </div>
+    </>
   )
 }

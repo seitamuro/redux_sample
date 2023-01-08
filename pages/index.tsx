34238bcccdb338f44432538c45caa1ea54ddef53
seitamuro <seitamuro0210@gmail.com>
2023-01-08 21:39:09 +0900

    page以外のコンポーネントはcomponentsフォルダに移動

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
-import store from "./app/store"
+import store from "../components/app/store"
 import { Provider } from "react-redux"
-import { Counter } from './feature/counter/Counter'
+import { Counter } from '../components/feature/counter/Counter'
 
 export default function Home() {
   return (

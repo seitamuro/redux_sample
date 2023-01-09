c292624f4dcae5f823c120399ec85ac875da250d
seitamuro <seitamuro0210@gmail.com>
2023-01-09 20:30:19 +0900

    Memoのカスタムホックを作成

--- components/feature/memo/Memo.tsx
+++ components/feature/memo/Memo.tsx
@@ -1,1 +1,18 @@
-export const useMemos = 1
+import {
+    selectMemos,
+    selectMemo,
+    add,
+    del,
+ } from "./memoSlice"
+import { useDispatch, useSelector } from "react-redux"
+
+export function useMemos() {
+    const memos = useSelector(selectMemos)
+    const getMemo = useSelector(selectMemo)
+    const dispatch = useDispatch()
+
+    const addMemo = (text: string) => dispatch(add(text));
+    const delMemo = (id: string) => dispatch(del(id))
+
+    return [memos, getMemo, addMemo, delMemo]
+}
